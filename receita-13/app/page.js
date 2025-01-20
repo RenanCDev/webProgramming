9b698df5d0323d330b5fd28ca11e1a868a18001e
Renan Costa <renan.costa.bsi@gmail.com>
2025-01-20 19:31:46 -0300

    pass 0

---
+++ receita-13/app/page.js
@@ -0,0 +1,12 @@
+export default function Home(){
+    return (
+        <div>
+            <div>Você é minha vida meu piquito</div>
+            <div>
+                <h1>
+                    Eu te amo mais que tusoooo nenzoooo
+                </h1>
+            </div>
+        </div>       
+    )
+}
