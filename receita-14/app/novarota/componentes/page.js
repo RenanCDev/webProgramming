de464cae387de00b99429cd972abce2bc0733e57
Renan Costa <renan.costa.bsi@gmail.com>
2025-01-21 16:51:21 -0300

    pass 2

---
+++ receita-14/app/novarota/componentes/page.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export function Home2() {
+  return (
+    <div>
+      <div>Menu principal2</div>
+      <div>
+        <h1>GRANDE REI EXU2</h1>
+      </div>
+    </div>
+  );
+}
+
+  
+  export function NewFunction2() {
+    return (
+      <div>
+          <div>Menu principal2</div>
+  
+          <div>
+              <h1>GRANDE REI EXU2</h1>
+          </div>
+      </div>
+      );
+  }
--- receita-14/app/novarota/page.js
+++ receita-14/app/novarota/page.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import { MariaPrea } from "./componentes";
 import { MariaPrea2 } from "./componentes2";
 import { MariaPrea3 } from "./componentes/componentes3";
+import { Home2 } from "./componentes/page";
+import { NewFunction } from "../page";
 
 
 
@@ -9,6 +12,9 @@ export default function NovaRotaHome(){
     return (
 
        <div>
+          
+          <NewFunction/>
+          <Home2/>
 
           <h1>Nova Rota, Nova Página</h1>
 
