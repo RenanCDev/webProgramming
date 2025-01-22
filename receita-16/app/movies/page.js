0cdca91020925b3ccf7ce2a647e676afb469f765
Renan Costa <renan.costa.bsi@gmail.com>
2025-01-21 23:50:01 -0300

    pass 1

--- receita-16/app/movies/page.js
+++ receita-16/app/movies/page.js
@@ -1,25 +1,31 @@
-export default async function Home({searchParams}){
-
-    const {titleSearchKey = 'bagdad'} = await searchParams
-
-    const res = await fetch(`http://www.omdbapi.com/?apikey=e0133284&s=${titleSearchKey}`)
-
-    const data = await res.json()
-
-
-
+export default async function Home({ searchParams }) {
+    const { 
+      titleSearchKey = 'bagdad', 
+      type = '',
+      year = '',
+      page = 1
+    } = searchParams;
+  
+    const res = await fetch(
+      `http://www.omdbapi.com/?apikey=e0133284&s=${titleSearchKey}&type=${type}&y=${year}&page=${page}`
+    );
+  
+    const data = await res.json();
+  
+    if (!data.Search) {
+      return <div>Não foram encontrados resultados para a pesquisa.</div>;
+    }
+  
     return (
-
+      <div>
         <div>
-
-            <div>
-
-            {data.Search.map( (m) => <div key={m.imdbID}>{m.Title} --- {m.Year}</div>  )}               
-
+          {data.Search.map((m) => (
+            <div key={m.imdbID}>
+              {m.Title} --- {m.Year}
             </div>
-
+          ))}
         </div>
-
-    )
-
-}
+      </div>
+    );
+  }
+  
