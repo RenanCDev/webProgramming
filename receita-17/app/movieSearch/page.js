20f83b3803eb6983682df989e23df31fbce017e7
Renan Costa <renan.costa.bsi@gmail.com>
2025-01-22 18:36:04 -0300

    pass 1

--- receita-17/app/movieSearch/page.js
+++ receita-17/app/movieSearch/page.js
@@ -1,19 +1,24 @@
 import Form from "next/form"
 
-export default async function MovieForm(){
+export default async function MovieForm() {
 
     return (
-
         <Form action="/movies">
+            <label htmlFor="idTitleSearchKey">Título</label>
+            <input id="idTitleSearchKey" name="titleSearchKey" />
 
-            <label htmlFor="idTitleSearchKey">Título</label>            
-
-            <input id="idTitleSearchKey" name="titleSearchKey"/>
+            <label htmlFor="idYearSearchKey">Ano</label>
+            <input
+                id="idYearSearchKey"
+                name="yearSearchKey"
+                type="number"
+                placeholder="YYYY"
+                min="1900"
+                max={new Date().getFullYear()}
+                required
+            />
 
             <button type="submit">Pesquisar</button>
-
         </Form>
-
-    )
-
+    );
 }
--- receita-17/app/movies/page.js
+++ receita-17/app/movies/page.js
@@ -1,39 +1,53 @@
 export default async function Home({ searchParams }) {
-    const { 
-      titleSearchKey = 'bagdad', 
-      type = '', 
-      year = '', 
-      page = 1 
-    } = await searchParams;
-  
-    const res = await fetch(
-      `http://www.omdbapi.com/?apikey=e0133284&s=${titleSearchKey}&type=${type}&y=${year}&page=${page}`
-    );
-  
-    const data = await res.json();
-  
-    if (!data.Search) {
-      return <div>Não foram encontrados resultados para a pesquisa.</div>;
-    }
-  
-    return (
-      <div>
-        <div>
-          {data.Search.map((m) => (
-            <div key={m.imdbID}>
-              <h3>{m.Title} --- {m.Year}</h3>
-              {m.Poster !== 'N/A' && (
-                <img 
-                  src={m.Poster} 
-                  alt={`${m.Title} Poster`} 
-                  style={{ width: '200px', height: 'auto' }} 
-                />
-              )}
-              {m.Poster === 'N/A' && <p>Sem pôster disponível</p>}
-            </div>
-          ))}
-        </div>
-      </div>
-    );
+  const { 
+    titleSearchKey = "bagdad", 
+    type = "", 
+    yearSearchKey = "", 
+    page = 1 
+  } = await searchParams;
+
+  const res = await fetch(
+    `http://www.omdbapi.com/?apikey=e0133284&s=${titleSearchKey}&type=${type}&page=${page}`
+  );
+
+  const data = await res.json();
+
+  if (!data.Search) {
+    return <div>Não foram encontrados resultados para a pesquisa.</div>;
   }
-  
+
+  const filteredMovies = yearSearchKey
+    ? data.Search.filter((movie) => {
+        const movieYear = parseInt(movie.Year, 10);
+        const startYear = Math.floor(parseInt(yearSearchKey, 10) / 10) * 10;
+        const endYear = startYear + 10;
+
+        return movieYear >= startYear && movieYear < endYear;
+      })
+    : data.Search;
+
+  if (filteredMovies.length === 0) {
+    return <div>Nenhum filme encontrado para a década especificada.</div>;
+  }
+
+  return (
+    <div>
+      {filteredMovies.map((m) => (
+        <div key={m.imdbID}>
+          <h3>
+            {m.Title} --- {m.Year}
+          </h3>
+          {m.Poster !== "N/A" ? (
+            <img
+              src={m.Poster}
+              alt={`${m.Title} Poster`}
+              style={{ width: "200px", height: "auto" }}
+            />
+          ) : (
+            <p>Sem pôster disponível</p>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
